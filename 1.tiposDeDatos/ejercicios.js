c723ffe5b61e699caf1f6ecefff4ecdf22822b33
maykol <maykolmarca@gmail.com>
2022-10-19 15:54:53 -0500

    tarea ralizada

--- 1.tiposDeDatos/ejercicios.js
+++ 1.tiposDeDatos/ejercicios.js
@@ -1,2 +1,23 @@
 // averiguar el methodo de javascript
-// que me permite saber que tipo de tengo
+// que me permite saber que tipo de dato tengo
+//RESPUESTA
+// el metodo es el operador typeof,que devuelve una cadena  de texto
+// que describe el tipo del operador  que estamos comprobando
+// El operador typeof devuelve una cadena que indica el tipo de
+// operando sin evaluarlo. operando es la cadena, variable, palabra
+//clave u objeto para el que se devolverá su tipo. Los paréntesis 
+//son opcionales.
+//Suponga que define las siguientes variables:
+var miFuncion = new Function("5+2")
+var forma = "redonda"
+var tamano = 1
+var hoy = new Date()
+//El operador typeof devuelve los siguientes resultados para estas variables
+typeof miFuncion === 'function'
+typeof forma === 'string'
+typeof tamano === 'number'
+typeof hoy === 'object'
+typeof noExiste === 'undefined'
+//Para las palabras clave true y null, el operador typeof devuelve los siguientes resultados:
+typeof true === 'boolean'
+typeof null === 'object'
--- 2.variables/ejercicios.js
+++ 2.variables/ejercicios.js
@@ -1,1 +1,14 @@
 // averiguar las diferencias entre let y var
+// RESPUESTA
+// LET
+//La instrucción let declara una variable de alcance local con 
+//ámbito de bloque(blockscope), la cual, opcionalmente, puede ser 
+//inicializada con algún valor.
+// VAR
+//Las declaraciones de variables, donde sea que ocurran, son 
+//procesadas antes de que cualquier otro código sea ejecutado. 
+//El ámbito de una variable declarada con la palabra reservada var
+ // es su contexto de ejecución en curso*,* que puede ser la 
+ //función que la contiene o, para las variables declaradas afuera
+  //de cualquier función, un ámbito global. Si re-declaras una 
+  //variable Javascript, esta no perderá su valor.
